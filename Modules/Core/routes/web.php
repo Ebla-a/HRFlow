da62518c94e374b998e926bb505f04d4da7a0a42
Ebla-a <eblaali520@.com>
2026-07-26 22:40:40 +0300

    fix: configure PSR-4 autoload for Modules namespace

--- Modules/Core/routes/web.php
+++ Modules/Core/routes/web.php
@@ -1,8 +1,16 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use Modules\Core\Http\Controllers\CoreController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('cores', CoreController::class)->names('core');
+Route::prefix('core')->group(function() {
+    Route::get('/', 'CoreController@index');
 });
