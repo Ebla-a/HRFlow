b9ed1c68a42b0bcaa82ee089a4e2917b844baa5a
Ebla-a <eblaali520@.com>
2026-07-27 23:04:09 +0300

    feat: add api response stantard + Exceptionhandler

---
+++ Modules/Core/App/Traits/ApiResponseTrait.php
@@ -0,0 +1,24 @@
+<?php
+namespace Modules\Core\App\Traits;
+
+class ApiResponseTrait {
+
+protected function success($data = null , $message="Success", $status = 200)
+{
+    return response()->json([
+           'status' => true,
+            'message' => $message,
+            'data' => $data,
+        ], $status);
+}
+
+
+    protected function error($message = 'Error', $status = 400, $errors = [])
+    {
+        return response()->json([
+            'status' => false,
+            'message' => $message,
+            'errors' => $errors,
+        ], $status);
+    }
+}
